175f09d03842970c9fea54de759f65e0de53c9ed
DML <leithdm@gmail.com>
2016-01-07 13:09:27 +0000

    Initial Commit

---
+++ LeavingCert2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeavingCert2016
+//
+//  Created by Darren Leith on 07/01/2016.
+//  Copyright © 2016 Darren Leith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
